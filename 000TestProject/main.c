9f5e6ea0e201a340faaa13b1f23d8380565e5a4a
diaaeldine <42654065+diaaeldine@users.noreply.github.com>
2022-12-14 23:21:21 -0800

    Add files via upload

---
+++ 000TestProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 17, 2022
+ *      Author: Diaa
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf("Hello World\n");
+	printf("Press 'Enter' to Exit this application\n");
+
+	getchar();
+	return 0;
+}
+
